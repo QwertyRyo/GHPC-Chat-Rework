a72b391705ca0091c8a0a9dfb2e073f706a89c15
Ryan Shen <ryanshenpek@gmail.com>
2026-07-13 18:36:06 -0700

    slkdhfkds

--- Chat_Rework.cs
+++ Chat_Rework.cs
@@ -2,11 +2,19 @@ using MelonLoader;
 using HarmonyLib;
 using GHPC;
 using GHPC.AI.Radios;
+using GHPC.Effects.Voices;
+using GHPC.Effects.Voices.VoiceRoutines;
+using GHPC.Crew;
 using GHPC.Player;
 using GHPC.UI.Hud;
 using UnityEngine;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Reflection.Emit;
+using System.Linq;
+using GHPC.Effects;
+using GHPC.AI.Interfaces;
+
 
 [assembly:MelonInfo(typeof(ChatReworkMod.ChatReworkModClass), "Chat Rework",
                     "1.0.0", "Qwertyryo")]
@@ -33,8 +41,20 @@ namespace ChatReworkMod {
 
   public class ChatReworkModClass : MelonMod {
     public override void OnInitializeMelon() {
+      MelonLogger.Msg("Chat Rework initializing...");
       Prefs.Register();
       HarmonyInstance.PatchAll();
+      var patched = HarmonyInstance.GetPatchedMethods();
+      foreach (var m in patched)
+        MelonLogger.Msg("[Patch] " + m.DeclaringType?.Name + "." + m.Name);
+      MelonLogger.Msg("Chat Rework initialized.");
+    }
+  }
+
+  public static class AllegianceHelper {
+    public static bool IsFriendly(Faction unitAllegiance) {
+      var player = PlayerInput.Instance?.CurrentPlayerUnit;
+      return player != null && player.Allegiance == unitAllegiance;
     }
   }
 
@@ -43,6 +63,8 @@ namespace ChatReworkMod {
   [HarmonyPatch(typeof(CommsTextMessageProcessor), "DisplaySpottingMessage")]
   public static class DisplaySpottingMessagePatch {
     internal static bool SkipNamePatch = false;
+    private static readonly Dictionary<string, float> _recentSpots = new Dictionary<string, float>();
+    private const float SpotWindow = 1.0f;
     public static int rough_range(float dist)
     {
       float t = Mathf.Clamp01((dist - 400f) / (2000f - 400f));
@@ -109,273 +131,223 @@ namespace ChatReworkMod {
       }
 
     }
-    private static string FormatEnemyName(string name, float dist) {
+    internal static string FormatName(string name) {
       switch (name) {
-          // units which don't need to be differentiated at this 600-1200m range
-
-        case "Infantry":
-          return "Infantry";
-        
+        case "Infantry":          return "Infantry";
 
         case "STATIC_TOW":
-        case "STATIC_TOW_TRENCH":
-          return "TOW";
+        case "STATIC_TOW_TRENCH":         return "TOW";
+
+        case "STATIC_SPG9_TRENCH":
+        case "STATIC_SPG9":               return "SPG-9";
 
         case "STATIC_9K111":
         case "STATIC_9K111_TRENCH":
         case "STATIC_9K111_SA_TRENCH":
-        case "STATIC_9K111_SA":
-          return "9K111";
+        case "STATIC_9K111_SA":           return "9K111";
 
         case "BRDM2_SA":
-        case "BRDM2":
-          return "BRDM-2";
-
-
+        case "BRDM2":                     return "BRDM-2";
 
         case "M2BRADLEY(ALT)":
-        case "M2BRADLEY":
-          return "M2 Bradley";
+        case "M2BRADLEY":                 return "M2 Bradley";
 
         case "BMP2_SA":
-        case "BMP2":
-          return "BMP-2";
-
+        case "BMP2":                      return "BMP-2";
 
         case "Mi24":
         case "Mi24_rockets":
         case "Mi24V_NVA":
         case "Mi24V_SA_rockets":
         case "Mi24V_NVA_rockets":
-        case "Mi24V_SA":
-          return "Mi-24";
+        case "Mi24V_SA":                  return "Mi-24";
 
-        case "OH58A":
-        return "OH-58A";
-        
-
-        case "Mi8T":
-          return "Mi-8T";
-
-        case "MI2":
-          return "Mi-2";
+        case "OH58A":                     return "OH-58A";
+        case "Mi8T":                      return "Mi-8T";
+        case "MI2":                       return "Mi-2";
 
         case "AH1":
-        case "AH1_rockets":
-          return "AH-1";
+        case "AH1_rockets":               return "AH-1";
 
         case "URAL375D_SA":
+        case "URAL375D":                  return "URAL 375D";
+
+        case "M923":                      return "M923";
+        case "T80B":                      return "T-80B";
+        case "T62":                       return "T-62";
+        case "PT76B":                     return "PT-76B";
+        case "UAZ469":                    return "UAZ-469";
+        case "T3485":                     return "T-34-85M";
+
+        case "BTR60PB_SA":
+        case "BTR60PB":                   return "BTR-60";
+        case "BTR70":                     return "BTR-70";
+
+        case "BMP1_SA":
+        case "BMP1":                      return "BMP-1";
+        case "BMP1P":
+        case "BMP1P_SA":                  return "BMP-1P";
+
+        case "LEO1A1":                    return "Leopard 1A1";
+        case "LEO1A1A2":                  return "Leopard 1A1A2";
+        case "LEO1A1A3":                  return "Leopard 1A1A3";
+        case "LEO1A1A4":                  return "Leopard 1A1A4";
+        case "LEO1A3":                    return "Leopard 1A3";
+        case "LEO1A3A1":                  return "Leopard 1A3A1";
+        case "LEO1A3A2":                  return "Leopard 1A3A2";
+        case "LEO1A3A3":                  return "Leopard 1A3A3";
+        case "LEO1A4":                    return "Leopard 1A4";
+
+        case "T64R":                      return "T-64R";
+        case "T64A74":                    return "T-64A 1974";
+        case "T64A79":                    return "T-64A 1979";
+        case "T64A81":                    return "T-64A 1981";
+        case "T64A":                      return "T-64A 1983";
+        case "T64A84":                    return "T-64A 1984";
+        case "T64B":                      return "T-64B 1984";
+        case "T64B81":                    return "T-64B 1981";
+        case "T64B1":                     return "T-64B1 1984";
+        case "T64B181":                   return "T-64B1 1981";
+
+        case "T72M":                      return "T-72M";
+        case "T72":                       return "T-72";
+        case "T72UV1":                    return "T-72 ÜV1";
+        case "T72UV2":                    return "T-72 ÜV2";
+        case "T72ULEM":                   return "T-72 LEM mod";
+        case "T72M1":                     return "T-72M1";
+        case "T72GILLS":                  return "T-72 LEM";
+
+        case "T54A":                      return "T-54A";
+        case "T55A":                      return "T-55A";
+
+        case "M60A1":                     return "M60A1";
+        case "M60A1AOS":                  return "M60A1AOS";
+        case "M60A3":                     return "M60A3";
+        case "M60A3TTS":                  return "M60A3 TTS";
+        case "M60A1RISEP77":              return "M60A1 Rise Passive '77";
+        case "M60A1RISEP":                return "M60A1 Rise Passive";
+
+        case "M1IP Abrams":                      return "M1IP";
+        case "M1":                        return "M1 Abrams";
+
+        case "MARDER1A2":                 return "Marder 1A2";
+        case "MARDERA1":
+        case "MARDERA1_NO_ATGM":          return "Marder A1-";
+        case "MARDERA1PLUS":              return "Marder A1+";
+
+        case "M113G":
+        case "M113":                      return "M113";
+
+        default:                          return name;
+      }
+    }
+
+
+    private static string IdentifyEnemy(string name, float dist) {
+      // Units always reported at full specificity regardless of distance.
+      switch (name) {
+        case "Infantry":
+        case "STATIC_TOW":
+        case "STATIC_TOW_TRENCH":
+        case "STATIC_SPG9_TRENCH":
+        case "STATIC_SPG9":
+        case "STATIC_9K111":
+        case "STATIC_9K111_TRENCH":
+        case "STATIC_9K111_SA_TRENCH":
+        case "STATIC_9K111_SA":
+        case "BRDM2_SA":
+        case "BRDM2":
+        case "M2BRADLEY(ALT)":
+        case "M2BRADLEY":
+        case "BMP2_SA":
+        case "BMP2":
+        case "Mi24":
+        case "Mi24_rockets":
+        case "Mi24V_NVA":
+        case "Mi24V_SA_rockets":
+        case "Mi24V_NVA_rockets":
+        case "Mi24V_SA":
+        case "OH58A":
+        case "Mi8T":
+        case "MI2":
+        case "AH1":
+        case "AH1_rockets":
+        case "URAL375D_SA":
         case "URAL375D":
-          return "URAL 375D";
-
         case "M923":
-            return "M923";
-
         case "T80B":
-          return "T-80B";
-
         case "T62":
-          return "T-62";
-
         case "PT76B":
-          return "PT-76B";
         case "UAZ469":
-          return "UAZ-469";
         case "T3485":
-          return "T-34-85M";
-
-
+          return FormatName(name);
       }
 
-      if (dist > Prefs.CloseRange.Value) {
-        switch (name) {
+      if (dist <= Prefs.CloseRange.Value)
+        return FormatName(name);
 
+      // Mid-range: report at group level.
+      switch (name) {
         case "BTR60PB_SA":
         case "BTR60PB":
-        case "BTR70":
-          return "BTR";
+        case "BTR70":                     return "BTR";
 
         case "BMP1_SA":
         case "BMP1":
         case "BMP1P":
-        case "BMP1P_SA":
-          return "BMP-1";
+        case "BMP1P_SA":                  return "BMP-1";
 
-          case "LEO1A1":
-          case "LEO1A1A2":
-          case "LEO1A1A3":
-          case "LEO1A1A4":
-          case "LEO1A3":
-          case "LEO1A3A1":
-          case "LEO1A3A2":
-          case "LEO1A3A3":
-          case "LEO1A4":
-            return "Leopard 1";
+        case "LEO1A1":
+        case "LEO1A1A2":
+        case "LEO1A1A3":
+        case "LEO1A1A4":
+        case "LEO1A3":
+        case "LEO1A3A1":
+        case "LEO1A3A2":
+        case "LEO1A3A3":
+        case "LEO1A4":                    return "Leopard 1";
 
-          case "T64R":
-          case "T64A74":
-          case "T64A79":
-          case "T64A81":
-          case "T64A":
-          case "T64A84":
-          case "T64B":
-          case "T64B81":
-          case "T64B1":
-          case "T64B181":
-            return "T-64";
+        case "T64R":
+        case "T64A74":
+        case "T64A79":
+        case "T64A81":
+        case "T64A":
+        case "T64A84":
+        case "T64B":
+        case "T64B81":
+        case "T64B1":
+        case "T64B181":                   return "T-64";
 
-          case "T72M":
-          case "T72":
-          case "T72UV1":
-          case "T72UV2":
-          case "T72ULEM":
-          case "T72M1":
-          case "T72GILLS":
-            return "T-72";
+        case "T72M":
+        case "T72":
+        case "T72UV1":
+        case "T72UV2":
+        case "T72ULEM":
+        case "T72M1":
+        case "T72GILLS":                  return "T-72";
 
-          case "T54A":
-          case "T55A":
-            return "T-55";
+        case "T54A":
+        case "T55A":                      return "T-55";
 
-          case "M60A1":
-          case "M60A1AOS":
-          case "M60A3":
-          case "M60A3TTS":
-          case "M60A1RISEP77":
-          case "M60A1RISEP":
-            return "M60";
+        case "M60A1":
+        case "M60A1AOS":
+        case "M60A3":
+        case "M60A3TTS":
+        case "M60A1RISEP77":
+        case "M60A1RISEP":                return "M60";
 
-          case "M1IP":
-          case "M1":
-            return "M1 Abrams";
+        case "M1IP":
+        case "M1":                        return "M1 Abrams";
 
-          case "MARDER1A2":
-          case "MARDERA1":
-          case "MARDERA1_NO_ATGM":
-          case "MARDERA1PLUS":
-            return "Marder 1";
+        case "MARDER1A2":
+        case "MARDERA1":
+        case "MARDERA1_NO_ATGM":
+        case "MARDERA1PLUS":              return "Marder 1";
 
         case "M113G":
-        case "M113":
-          return "M113";
+        case "M113":                      return "M113";
 
-
-          default:
-            return $"{name}";
-        }
-      } else {
-        // close range
-        switch (name) {
-
-
-        case "BTR60PB_SA":
-        case "BTR60PB":
-          return "BTR-60";
-        case "BTR70":
-          return "BTR-70";
-        case "BMP1_SA":
-        case "BMP1":
-                  return "BMP-1";
-        case "BMP1P":
-        case "BMP1P_SA":
-        return "BMP-1P";
-
-          case "LEO1A1":
-            return "Leopard 1A1";
-          case "LEO1A1A2":
-            return "Leopard 1A1A2";
-          case "LEO1A1A3":
-            return "Leopard 1A1A3";
-          case "LEO1A1A4":
-            return "Leopard 1A1A4";
-          case "LEO1A3":
-            return "Leopard 1A3";
-          case "LEO1A3A1":
-            return "Leopard 1A3A1";
-          case "LEO1A3A2":
-            return "Leopard 1A3A2";
-          case "LEO1A3A3":
-            return "Leopard 1A3A3";
-          case "LEO1A4":
-            return "Leopard 1A4";
-
-          case "T64R":
-            return "T-64R";
-          case "T64A74":
-            return "T-64A 1974";
-          case "T64A79":
-            return "T-64A 1979";
-          case "T64A81":
-            return "T-64A 1981";
-          case "T64A":
-            return "T-64A 1983";
-          case "T64A84":
-            return "T-64A 1984";
-          case "T64B":
-            return "T-64B 1984";
-          case "T64B81":
-            return "T-64B 1981";
-          case "T64B1":
-            return "T-64B1 1984";
-          case "T64B181":
-            return "T-64B1 1981";
-
-          case "T72M":
-            return "T-72M";
-          case "T72":
-            return "T-72";
-          case "T72UV1":
-            return "T-72 ÜV1";
-          case "T72UV2":
-            return "T-72 ÜV2";
-
-          case "T72ULEM":
-            return "T-72 LEM mod";
-          case "T72M1":
-            return "T-72M1";
-          case "T72GILLS":
-            return "T-72 LEM";
-
-          case "T54A":
-            return "T-54A";
-          case "T55A":
-            return "T-55A";
-
-          case "M60A1":
-            return "M60A1";
-          case "M60A1AOS":
-            return "M60A1AOS";
-          case "M60A3":
-            return "M60A3";
-          case "M60A3TTS":
-            return "M60A3 TTS";
-          case "M60A1RISEP77":
-            return "M60A1 Rise Passive '77";
-          case "M60A1RISEP":
-            return "M60A1 Rise Passive";
-
-          case "M1IP":
-            return "M1IP Abrams";
-          case "M1":
-            return "M1 Abrams";
-
-          case "MARDER1A2":
-            return "Marder 1A2";
-          case "MARDERA1":
-          case "MARDERA1_NO_ATGM":
-            return "Marder A1-";
-          case "MARDERA1PLUS":
-            return "Marder A1+";
-
-            
-        case "M113G":
-            return "M113";
-        case "M113":
-          return "M113";
-
-          default:
-            return $"DEBUG: ${name}$";
-        }
+        default:                          return FormatName(name);
       }
     }
 
@@ -384,12 +356,17 @@ namespace ChatReworkMod {
                        SpottingData spotter, SpottingData spotted) {
       if (spotter.Unit == PlayerInput.Instance.CurrentPlayerUnit)
         return false;
+      string spotKey = spotter.Unit.GetInstanceID() + "_" + spotted.Unit.GetInstanceID();
+      float now = Time.time;
+      if (_recentSpots.TryGetValue(spotKey, out float lastSpot) && now - lastSpot < SpotWindow)
+        return false;
+      _recentSpots[spotKey] = now;
       Vector3 position = spotter.Unit.transform.position;
       Vector3 vector = spotted.Unit.transform.position - position;
       vector.y = 0f;
       float dist = vector.magnitude;
       string text2 = "";
-      MelonLogger.Msg($"Spotting Dist {dist}");
+      //MelonLogger.Msg($"Spotting Dist {dist}");
       if (dist > Prefs.MidRange.Value) {
         string shortText = spotted.Unit.ShortNameUs.ToString();
         if (shortText == "Pc") shortText = "PC";
@@ -404,7 +381,7 @@ namespace ChatReworkMod {
       }
     else
             {
-            text2 = FormatEnemyName(spotted.Unit.UniqueName, dist);
+            text2 = IdentifyEnemy(spotted.Unit.UniqueName, dist);
             text2 = string.Concat(new string[]
 			{
 				"<color=#",
@@ -427,5 +404,54 @@ namespace ChatReworkMod {
     }
   }
 
+  [HarmonyPatch(typeof(CrewManager), "DoEvacuationCoroutine")]
+  public static class DoEvacuationCoroutinePatch {
+    private static readonly Dictionary<CrewManager, float> _recent = new Dictionary<CrewManager, float>();
+    private const float Window = 0.5f;
+
+    [HarmonyPostfix]
+    static void Postfix(CrewManager __instance) {
+      if (!AllegianceHelper.IsFriendly(__instance.Unit.Allegiance)) return;
+      float now = Time.time;
+      if (_recent.TryGetValue(__instance, out float last) && now - last < Window) return;
+      _recent[__instance] = now;
+      CommsTextMessageProcessor i = CommsTextMessageProcessor.I;
+      string text2 = "We're bailing out.";
+      i.DisplayCustomMessage(DisplaySpottingMessagePatch.FormatName(__instance.Unit.UniqueName), text2);
+    }
+  }
+
+  [HarmonyPatch(typeof(CrewManager), "KillAllRemainingCrew")]
+  public static class KillAllRemainingCrewPatch {
+    private static readonly Dictionary<CrewManager, float> _recent = new Dictionary<CrewManager, float>();
+    private const float Window = 0.5f;
+
+    [HarmonyPostfix]
+    static void Postfix(CrewManager __instance) {
+      if (!AllegianceHelper.IsFriendly(__instance.Unit.Allegiance)) return;
+      float now = Time.time;
+      if (_recent.TryGetValue(__instance, out float last) && now - last < Window) return;
+      _recent[__instance] = now;
+      CommsTextMessageProcessor i = CommsTextMessageProcessor.I;
+      string text2 = "AAAAAHH-";
+      i.DisplayCustomMessage(__instance.Unit.UniqueName, text2);
+    }
+  }
+
+  [HarmonyPatch(typeof(VoicePlayer), "PlayAudioEvent")]
+  public static class VoicePlayerPlayAudioEventPatch {
+    [HarmonyPrefix]
+    static void Prefix(VoicePlayer __instance, CrewPosition crew, string eventKey) {
+      MelonLogger.Msg($"[PlayAudioEvent] crew={crew} key={eventKey}");
+    }
+  }
+
+  [HarmonyPatch(typeof(VoiceRoutinesBase), "TargetStruck")]
+  public static class VoiceRoutinesBaseTargetStruckPatch {
+    [HarmonyPrefix]
+    static void Prefix(VoiceRoutinesBase __instance, ITarget target) {
+      MelonLogger.Msg($"[TargetStruck] target={target?.Owner?.UniqueName ?? "null"}");
+    }
+  }
 
 }
